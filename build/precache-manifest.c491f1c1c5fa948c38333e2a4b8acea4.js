56859abcfd13eae863d84f1c962151dfbb525dc6
Muhammad Vickramdani <muhammadvicramdani@gmail.com>
2021-02-16 20:55:40 +0700

    adding build

---
+++ build/precache-manifest.c491f1c1c5fa948c38333e2a4b8acea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac1af29b2fc5ba8ad7c65fb1189c08b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c55ac1ea5e76f43251",
+    "url": "/static/css/main.4133e790.chunk.css"
+  },
+  {
+    "revision": "2d5fd7cccc961a4696b4",
+    "url": "/static/js/2.d719929a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d719929a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c55ac1ea5e76f43251",
+    "url": "/static/js/main.fe55abc0.chunk.js"
+  },
+  {
+    "revision": "147a8a57eacf3aefb55b",
+    "url": "/static/js/runtime-main.d00a8b79.js"
+  }
+]);
